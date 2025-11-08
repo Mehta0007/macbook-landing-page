acaffdcbd5ae97ae6a4acd8c28d08fc258366c05
Ankit Mehta <ankitmmehta08@gmail.com>
2025-11-08 13:25:06 +0530

    added hero section

--- macbook_pro/src/components/Hero.jsx
+++ macbook_pro/src/components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Hero = () => {
+
+    const videoRef = useRef()
+
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, [])
+
+
   return (
-    <div>Hero</div>
-    
+    <section id='hero'>
+        <div>
+            <h1>MacBook Pro </h1>
+            <img src="/title.png" alt="macbook title" />
+        </div>
+
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline  ></video>
+
+    <button>Buy</button>
+
+    <p>From $1599 or $133/mo for 12 months</p>
+
+
+
+    </section>
+
   )
 }
 
